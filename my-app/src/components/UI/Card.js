3f286ab6f95c7588a6940e4cabd7643e10e4d09c
Liubou Kavaleuskaya <sauchanka.liubou@gmail.com>
2021-07-30 18:20:30 +0300

    add app components and their styles

---
+++ my-app/src/components/UI/Card.js
@@ -0,0 +1,12 @@
+import './Card.css';
+
+function Card(props) {
+  return (
+    <div className="container__card" id={props.id}>
+      <h2 className="card__heading">{props.heading}</h2>
+      <p className="card__text">{props.text}</p>
+    </div>
+  );
+}
+
+export default Card;
---
+++ my-app/src/components/UI/Container.js
@@ -0,0 +1,7 @@
+import './Container.css';
+
+function Container(props) {
+  return <div className={props.className}>{props.children}</div>;
+}
+
+export default Container;
