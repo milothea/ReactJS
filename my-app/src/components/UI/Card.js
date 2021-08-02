c8bcd24214e23bf861c888b65ac06a06c2b1561c
Liubou Kavaleuskaya <sauchanka.liubou@gmail.com>
2021-08-03 00:48:21 +0300

    add checkbox event listener to change styles

--- my-app/src/components/UI/Card.js
+++ my-app/src/components/UI/Card.js
@@ -1,11 +1,27 @@
+import { useState } from 'react';
+import Container from './Container';
+import Checkbox from './Checkbox';
 import './Card.css';
 
 const Card = (props) => {
+  const [className, setClassName] = useState('container card');
+
+  function checkboxHandler(event) {
+    if (event.target.checked) {
+      setClassName('container card active');
+    } else {
+      setClassName('container card');
+    }
+  }
+
   return (
-    <div className="container__card" id={props.id}>
-      <h2 className="card__heading">{props.heading}</h2>
+    <Container className={className} id={props.id}>
+      <Container className='container heading'>
+        <h2 className="card__heading">{props.heading}</h2>
+        <Checkbox className='card__checkbox' type='checkbox' onChange={checkboxHandler} />
+      </Container>
       <p className="card__text">{props.text}</p>
-    </div>
+    </Container>
   );
 }
 
