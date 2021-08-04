e964fad2899f718aaf31f868a6c745e7e2a35014
Liubou Kavaleuskaya <sauchanka.liubou@gmail.com>
2021-08-05 00:18:53 +0300

    fix according to review comments

--- my-app/src/components/UI/Card.js
+++ my-app/src/components/UI/Card.js
@@ -3,71 +3,65 @@ import { HiOutlineCheck, HiOutlinePencil, HiOutlineX } from 'react-icons/hi';
 import './Card.css';
 
 const Card = (props) => {
-  const [checked, setChecked] = useState(false);
-  const [isEditMode, setEditMode] = useState(false);
-  const [headingValue, setHeadingValue] = useState(props.heading);
-  const [defaultHeading, setDefaultHeading] = useState(props.heading);
-  const [textValue, setTextValue] = useState(props.text);
-  const [defaultText, setDefaultText] = useState(props.text);
+    const [checked, setChecked] = useState(false);
+    const [isEditMode, setEditMode] = useState(false);
+    const [headingInput, setHeadingInput] = useState(props.heading);
+    const [defaultHeading, setDefaultHeading] = useState(props.heading);
+    const [textInput, setTextInput] = useState(props.text);
+    const [defaultText, setDefaultText] = useState(props.text);
 
-  const checkboxHandler = () => setChecked(!checked);
-  const editHandler = () => setEditMode(!isEditMode);
-  const inputHandler = (event) => {
-    const target = event.target;
+    const checkboxHandler = () => setChecked(!checked);
+    const editHandler = () => {
+        setChecked(false);
+        setEditMode(true);
+    };
+    const headingInputHandler = (event) => setHeadingInput(event.target.value);
+    const textInputHandler = (event) => setTextInput(event.target.value);
+    const submitHandler = () => {
+        setEditMode(false);
+        setDefaultHeading(headingInput);
+        setDefaultText(textInput);
+    };
+    const cancelHandler = () => {
+        setEditMode(false);
+        setHeadingInput(defaultHeading);
+        setTextInput(defaultText);
+    };
 
-    (target.id === 'heading') ? setHeadingValue(target.value) : setTextValue(target.value);
-  };
-  const submitHandler = () => {
-    setChecked(false);
-    editHandler();
-    setDefaultHeading(headingValue);
-    setDefaultText(textValue);
-  };
-  const cancelHandler = () => {
-    setChecked(false);
-    editHandler();
-    setHeadingValue(defaultHeading);
-    setTextValue(defaultText);
-  };
-
-  if (isEditMode) {
-    return (
-      <div className='container card editmode' id={props.id}>
-        <div className='container heading'>
-          <input className='card__heading'
-                 id='heading'
-                 value={headingValue}
-                 onChange={inputHandler}
-                 maxLength='15'/>
-          <HiOutlineCheck className='card__submit'
-                          onClick={submitHandler} />
-          <HiOutlineX className='card__cancel'
-                      onClick={cancelHandler} />
+    return isEditMode ? (
+        <div className='container card editmode' id={props.id}>
+            <div className='container heading'>
+                <input className='card__heading'
+                       id='heading'
+                       value={headingInput}
+                       onChange={headingInputHandler}
+                       maxLength='15'/>
+                <HiOutlineCheck className='card__submit'
+                                onClick={submitHandler} />
+                <HiOutlineX className='card__cancel'
+                            onClick={cancelHandler} />
+            </div>
+            <textarea className='card__text'
+                      onChange={textInputHandler}
+                      value={textInput}
+                      maxLength='140' />
         </div>
-        <textarea className='card__text'
-                  onChange={inputHandler}
-                  value={textValue}
-                  maxLength='140' />
-      </div>
+    ) : (
+        <div className={`container card${checked ? ' active' : ''}`}
+             id={props.id}>
+            <div className='container heading'>
+                <h2 className='card__heading'>{defaultHeading}</h2>
+                <HiOutlinePencil className='card__edit'
+                                 fill='red'
+                                 onClick={editHandler} />
+                <input className='card__checkbox'
+                       type='checkbox'
+                       onChange={checkboxHandler}
+                       value={checked} />
+            </div>
+            <p className='card__text'>{defaultText}</p>
+        </div>
     );
-  }
-
-  return (
-    <div className={`container card${(checked && !isEditMode) ? ' active' : ''}`}
-         id={props.id}>
-      <div className='container heading'>
-        <h2 className='card__heading'>{defaultHeading}</h2>
-        <HiOutlinePencil className='card__edit'
-                         fill='red'
-                         onClick={editHandler} />
-        <input className='card__checkbox'
-               type='checkbox'
-               onChange={checkboxHandler}
-               value={checked} />
-      </div>
-     <p className='card__text'>{defaultText}</p>
-    </div>
-  );
 }
 
 export default Card;
