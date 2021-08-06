2c9a37eaacb0024e8d1cd7405e65dac0bedbea0f
Liubou Kavaleuskaya <sauchanka.liubou@gmail.com>
2021-08-06 16:45:10 +0300

    pass cards data as prop to App

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import CardsData from './data/CardsData';
 import App from './App';
 
 ReactDOM.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
-    document.getElementById('root')
+  <React.StrictMode>
+    <App items={CardsData}/>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
