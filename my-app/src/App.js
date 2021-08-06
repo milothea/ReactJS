ccb510d091bff07ea4b8f5932376d206233bcb80
Liubou Kavaleuskaya <sauchanka.liubou@gmail.com>
2021-08-06 16:45:51 +0300

    implement view mode

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,17 +1,34 @@
 import './App.css';
-import CardsData from './data/CardsData';
+import { useState } from 'react';
 import Header from './components/UI/Header';
 import Card from './components/UI/Card';
 
-const App = () => {
-    return (
-        <div className='react-app'>
-            <Header title='header' />
-            <div className='container'>
-                {CardsData.map(data => <Card key={data.id} heading={data.heading} text={data.text} id={data.id} /> )}
-            </div>
-        </div>
-    );
+const App = (props) => {
+  const [checked, setChecked] = useState(false);
+  const containerClassName = `cards-container${checked ? ' viewmode' : ''}`;
+
+  const checkboxHandler = () => setChecked(!checked);
+
+  return (
+    <div className='react-app'>
+      <Header title='Notes' />
+      <div className='viewmode-control'>
+        <input type='checkbox' id='vm-controller' onClick={checkboxHandler}/>
+        <label className='vm-controller__label' htmlFor='vm-controller'>Read only</label>
+      </div>
+      <div className={containerClassName}>
+        {
+          props.items.map(item => {
+            return(<Card key={item.id}
+                        heading={item.heading}
+                        text={item.text}
+                        id={item.id}
+                        data-view-mode={checked}/>)
+          })
+        }
+      </div>
+    </div>
+  );
 }
 
 export default App;
