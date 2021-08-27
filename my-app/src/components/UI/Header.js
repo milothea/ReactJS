bab50abbab9aaee433c144e5118b528e8ed398ad
Liubou Kavaleuskaya <sauchanka.liubou@gmail.com>
2021-08-27 08:45:53 +0300

    add cards counter

--- my-app/src/components/UI/Header.js
+++ my-app/src/components/UI/Header.js
@@ -1,9 +1,20 @@
+import { useContext } from 'react';
+import AppContext from '../../data/app-context';
 import './Header.css';
 
+
 const Header = ({ title }) => {
+    const context = useContext(AppContext);
     return (
         <header className="App-header">
             <h1 className="App-header__heading">{title}</h1>
+            <div className='App-header__counter-wrapper'>
+                <h3>Total cards:
+                    <span className='App-header__counter'>
+                        {context.cardsData.length}
+                    </span>
+                </h3>
+            </div>
         </header>
     );
 }
