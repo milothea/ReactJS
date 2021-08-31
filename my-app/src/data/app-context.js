b24c1da9e5138878870f6a3ed2b1ee7d46193ef7
Liubou Kavaleuskaya <sauchanka.liubou@gmail.com>
2021-08-31 23:44:10 +0300

    refactor to upload cards data when user logged in

--- my-app/src/data/app-context.js
+++ my-app/src/data/app-context.js
@@ -1,41 +1,48 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import {v4 as uuidv4} from 'uuid';
 
 const AppContext = React.createContext({
     cardsData: [],
+    isLoggedIn: false,
     paths: {},
     onChangeActiveState: (id) => {},
     onUpdateCardData: (id, newHeading, newText) => {},
     onAddCard: (heading, text) => {},
-    onDeleteCard: () => {}
+    onDeleteCard: () => {},
+    onSignIn: () => {}
 });
 
 export const AppContextProvider = (props) => {
     const [cardsData, setCardsData] = useState([]);
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
     const paths = {
         mainPage: '/home',
         errorPage: '/error',
-        authPage: '/signin'
+        authPage: '/authorisation'
     }
 
     useEffect(() => {
-        axios.get('https://raw.githubusercontent.com/BrunnerLivio/PokemonDataGraber/master/output.json')
-            .then((res) => {
-                let data = [...res.data.slice(0, 15)];
+        if (isLoggedIn) {
+            axios.get('https://raw.githubusercontent.com/BrunnerLivio/PokemonDataGraber/master/output.json')
+                .then((res) => {
+                    let data = [...res.data.slice(0, 15)];
 
-                data = data.map((datum) => {
-                    const newDatum = {...datum};
+                    data = data.map((datum) => {
+                        const newDatum = {...datum};
 
-                    newDatum.isActive = false;
+                        newDatum.isActive = false;
 
-                    return newDatum
-                });
+                        return newDatum
+                    });
 
-                setCardsData(data);
-            })
-            .catch((err) => new Error(`Something went wrong. Error: ${err}`));
-    }, []);
+                    setCardsData(data);
+                })
+                .catch((err) => new Error(`Something went wrong. Error: ${err}`));
+        }
+    }, [isLoggedIn]);
+
+    const signInHelper = () => setIsLoggedIn((prevState) => !prevState);
 
     const changeActiveStateHandler = (id) => setCardsData(prevData => prevData.map(card => {
         if (card.Number === id) {
@@ -78,11 +85,13 @@ export const AppContextProvider = (props) => {
     return <AppContext.Provider value={
         {
             cardsData: cardsData,
+            isLoggedIn: isLoggedIn,
             paths: paths,
             onChangeActiveState: changeActiveStateHandler,
             onUpdateCardData: updateCardDataHandler,
             onAddCard: addCardHandler,
-            onDeleteCard: deleteCardHandler
+            onDeleteCard: deleteCardHandler,
+            onSignIn: signInHelper
         }
     }>
         {props.children}
